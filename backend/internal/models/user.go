8578ad8674957e9148b80ae7cbe055662ee057da
berylatieno <berylatieno30@gmail.com>
2025-08-04 16:42:22 +0300

    auth routes

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -9,11 +9,11 @@ import (
 type User struct {
 	gorm.Model
 
-	FirstName     string `gorm:"size:50;not null"`
-	LastName      string `gorm:"size:50;not null"`
-	Email         string `gorm:"not null"`
-	RoleID        uint
-	Password_hash string `gorm:"not null"`
-	IsActive      bool   `gorm:"default:true"`
-	LastLoginAt   *time.Time
+	FirstName    string `gorm:"size:50;not null"`
+	LastName     string `gorm:"size:50;not null"`
+	Email        string `gorm:"not null"`
+	RoleID       uint
+	PasswordHash string `gorm:"not null"`
+	IsActive     bool   `gorm:"default:true"`
+	LastLoginAt  *time.Time
 }
