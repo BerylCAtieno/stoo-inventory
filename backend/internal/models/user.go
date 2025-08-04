53f14ecef02f55c4ecb5c225e9cbf4883ec1ae71
berylatieno <berylatieno30@gmail.com>
2025-08-04 14:42:05 +0300

    feat: user model

---
+++ backend/internal/models/user.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+
+	FirstName     string `gorm:"size:50;not null"`
+	LastName      string `gorm:"size:50;not null"`
+	Email         string `gorm:"not null"`
+	RoleID        uint
+	Password_hash string `gorm:"not null"`
+	IsActive      bool   `gorm:"default:true"`
+	LastLoginAt   *time.Time
+}
