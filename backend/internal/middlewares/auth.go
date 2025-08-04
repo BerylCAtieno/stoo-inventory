a5032492ddbee25f552436b97d5e39e732702ff7
berylatieno <berylatieno30@gmail.com>
2025-08-04 20:41:51 +0300

    auth middleware

---
+++ backend/internal/middlewares/auth.go
@@ -0,0 +1,45 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/berylCAtieno/stoo-inventory/internal/config"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		header := c.GetHeader("Authorization")
+		if header == "" || !strings.HasPrefix(header, "Bearer ") {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Missing or invalid Authorization header"})
+			return
+		}
+
+		tokenString := strings.TrimPrefix(header, "Bearer ")
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method")
+			}
+			return []byte(config.Config.JwtSecret), nil
+		})
+
+		if err != nil || !token.Valid {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Invalid token claims"})
+			return
+		}
+
+		userID := uint(claims["user_id"].(float64))
+		c.Set("userID", userID)
+
+		c.Next()
+	}
+}
