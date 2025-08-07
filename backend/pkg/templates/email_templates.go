9317f8439de74f501e86d77992487dae21992b13
berylatieno <berylatieno30@gmail.com>
2025-08-07 17:36:41 +0300

    email templates

--- backend/pkg/templates/email_templates.go
+++ backend/pkg/templates/email_templates.go
@@ -1,1 +1,30 @@
 package templates
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"path/filepath"
+)
+
+type EmailData struct {
+	UserEmail string
+	OTP       string
+}
+
+func LoadTemplate(templateName string, data EmailData) (string, error) {
+	templatePath := filepath.Join("pkg", "templates", "templates", templateName)
+
+	tmpl, err := template.ParseFiles(templatePath)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse template: %w", err)
+	}
+
+	var body bytes.Buffer
+	err = tmpl.Execute(&body, data)
+	if err != nil {
+		return "", fmt.Errorf("failed to execute template: %w", err)
+	}
+
+	return body.String(), nil
+}
