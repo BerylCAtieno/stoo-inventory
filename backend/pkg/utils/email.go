41ec7577d7558a0a0610582ce2206fc235186e23
berylatieno <berylatieno30@gmail.com>
2025-08-12 16:21:32 +0300

    email sending logic

--- backend/pkg/utils/email.go
+++ backend/pkg/utils/email.go
@@ -2,19 +2,59 @@ package utils
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/berylCAtieno/stoo-inventory/internal/config"
 	"github.com/berylCAtieno/stoo-inventory/pkg/templates"
+	"gopkg.in/gomail.v2"
 )
 
-func SendPasswordResetEmail(email, otp string) {
+func SendPasswordResetEmail(email, otp string) error {
 	body, err := templates.LoadTemplate("reset_password.html", templates.EmailData{
 		UserEmail: email,
 		OTP:       otp,
 	})
 	if err != nil {
-		// Handle error (log or return)
+		return fmt.Errorf("failed to load password reset email template: %w", err)
 	}
 
-	// Use `body` in your email-sending logic
-	fmt.Println(body)
+	subject := "Password Reset Request"
+	return sendEmail(email, subject, body)
+}
+
+func SendVerificationEmail(email, otp string) error {
+	body, err := templates.LoadTemplate("verify_account.html", templates.EmailData{
+		UserEmail: email,
+		OTP:       otp,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to load verification email template: %w", err)
+	}
+
+	subject := "Verify Your Account"
+	return sendEmail(email, subject, body)
+}
+
+func sendEmail(to, subject, body string) error {
+
+	from := config.Config.MailAddress
+	password := config.Config.SMTPPassword
+	smtpHost := config.Config.SMTPHost
+	smtpPort := config.Config.SMTPPort
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", from)
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", subject)
+	m.SetBody("text/html", body)
+
+	d := gomail.NewDialer(smtpHost, smtpPort, from, password)
+
+	if err := d.DialAndSend(m); err != nil {
+		log.Printf("Error sending email to %s: %v", to, err)
+		return err
+	}
+
+	log.Printf("Email sent successfully to %s", to)
+	return nil
 }
