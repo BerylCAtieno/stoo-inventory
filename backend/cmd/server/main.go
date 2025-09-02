4c98b389947641754171cadf26f66f31599b3b83
berylatieno <berylatieno30@gmail.com>
2025-09-02 18:56:46 +0300

    add auth routes to app

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -8,10 +8,13 @@ import (
 	"github.com/berylCAtieno/stoo-inventory/cmd/migration"
 	"github.com/berylCAtieno/stoo-inventory/internal/config"
 	"github.com/berylCAtieno/stoo-inventory/internal/database"
+	"github.com/berylCAtieno/stoo-inventory/internal/handlers"
+	"github.com/berylCAtieno/stoo-inventory/internal/repositories"
+	"github.com/berylCAtieno/stoo-inventory/internal/routes"
+	"github.com/berylCAtieno/stoo-inventory/internal/services"
 )
 
 func main() {
-
 	config.LoadConfig()
 	database.Connect()
 	migration.Run()
@@ -27,12 +30,17 @@ func main() {
 		api.GET("/health", func(c *gin.Context) {
 			c.JSON(200, gin.H{"status": "OK"})
 		})
+
+		userRepo := repositories.NewUserRepository(database.DB)
+		authService := services.NewAuthService(userRepo)
+		authHandler := handlers.NewAuthHandler(authService)
+
+		routes.RegisterAuthRoutes(api, authHandler)
 	}
 
 	addr := ":" + config.Config.Port
 	log.Println("Server running on", addr)
-	err := router.Run(addr)
-	if err != nil {
+	if err := router.Run(addr); err != nil {
 		log.Fatal("Server failed to start:", err)
 	}
 }
